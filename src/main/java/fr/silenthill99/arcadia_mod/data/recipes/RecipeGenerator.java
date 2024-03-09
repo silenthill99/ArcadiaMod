bb3ab2872accea19d6d857bd43e3ac6d89642eb4
silenthill99 <florian.graziani@sfr.fr>
2024-03-09 11:01:49 +0100

    Rajout de recettes dans le Data Generator

--- src/main/java/fr/silenthill99/arcadia_mod/data/recipes/RecipeGenerator.java
+++ src/main/java/fr/silenthill99/arcadia_mod/data/recipes/RecipeGenerator.java
@@ -5,6 +5,7 @@ import fr.silenthill99.arcadia_mod.init.ModItems;
 import net.minecraft.advancements.criterion.InventoryChangeTrigger;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.*;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.tags.ItemTags;
 
@@ -52,43 +53,44 @@ public class RecipeGenerator extends RecipeProvider
                 .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(ModItems.RUBY_GEM.get()))
                 .save(consumer);
 
+        Item CHROMIUM = ModItems.CHROMIUM_INGOT.get();
         ShapedRecipeBuilder.shaped(ModItems.CHROMIUM_AXE.get())
                 .pattern("00")
                 .pattern("01")
                 .pattern(" 1")
-                .define('0', ModItems.CHROMIUM_INGOT.get())
+                .define('0', CHROMIUM)
                 .define('1', Items.STICK)
-                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(ModItems.CHROMIUM_INGOT.get(), Items.STICK))
+                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(CHROMIUM, Items.STICK))
                 .save(consumer);
 
         ShapedRecipeBuilder.shaped(ModBlocks.CHROMIUM_BLOCK.get())
                 .pattern("000")
                 .pattern("000")
                 .pattern("000")
-                .define('0', ModItems.CHROMIUM_INGOT.get())
-                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(ModItems.CHROMIUM_INGOT.get()))
+                .define('0', CHROMIUM)
+                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(CHROMIUM))
                 .save(consumer);
 
         ShapedRecipeBuilder.shaped(ModItems.CHROMIUM_BOOTS.get())
                 .pattern("0 0")
                 .pattern("0 0")
-                .define('0', ModItems.CHROMIUM_INGOT.get())
-                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(ModItems.CHROMIUM_INGOT.get()))
+                .define('0', CHROMIUM)
+                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(CHROMIUM))
                 .save(consumer);
 
         ShapedRecipeBuilder.shaped(ModItems.CHROMIUM_CHESTPLATE.get())
                 .pattern("0 0")
                 .pattern("000")
                 .pattern("000")
-                .define('0', ModItems.CHROMIUM_INGOT.get())
-                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(ModItems.CHROMIUM_INGOT.get()))
+                .define('0', CHROMIUM)
+                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(CHROMIUM))
                 .save(consumer);
 
         ShapedRecipeBuilder.shaped(ModItems.CHROMIUM_HELMET.get())
                 .pattern("000")
                 .pattern("0 0")
-                .define('0', ModItems.CHROMIUM_INGOT.get())
-                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(ModItems.CHROMIUM_INGOT.get()))
+                .define('0', CHROMIUM)
+                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(CHROMIUM))
                 .save(consumer);
 
         ShapedRecipeBuilder.shaped(ModItems.CHROMIUM_HOE.get())
@@ -98,6 +100,37 @@ public class RecipeGenerator extends RecipeProvider
                 .define('0', ModItems.CHROMIUM_HOE.get())
                 .define('1', Items.STICK)
                 .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(ModItems.CHROMIUM_HOE.get(), Items.STICK))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(ModItems.CHROMIUM_LEGGINGS.get())
+                .pattern("000")
+                .pattern("0 0")
+                .pattern("0 0")
+                .define('0', CHROMIUM)
+                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(CHROMIUM))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(ModItems.CHROMIUM_PICKAXE.get())
+                .pattern("000")
+                .pattern(" 1 ")
+                .pattern(" 1 ")
+                .define('0', CHROMIUM)
+                .define('1', Items.STICK)
+                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(CHROMIUM, Items.STICK))
+                .save(consumer);
+
+        ShapelessRecipeBuilder.shapeless(CHROMIUM, 9)
+                .requires(ModBlocks.CHROMIUM_BLOCK.get())
+                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(ModBlocks.CHROMIUM_BLOCK.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(ModItems.CHROMIUM_SHOVEL.get())
+                .pattern("0")
+                .pattern("1")
+                .pattern("1")
+                .define('0', CHROMIUM)
+                .define('1', Items.STICK)
+                .unlockedBy("unlock", InventoryChangeTrigger.Instance.hasItems(CHROMIUM, Items.STICK))
                 .save(consumer);
     }
 }
